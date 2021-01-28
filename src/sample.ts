d14b998d5d9b492a221ef499262aae681a4261c5
Michael Huelsen <github@michael-huelsen.de>
2021-01-28 21:53:28 +0100

    updated

--- src/sample.ts
+++ src/sample.ts
@@ -23,17 +23,46 @@ function assertUserApiResult(obj: any): asserts obj is MyApiResult<MyUser> {
     }
 }
 
-async function fetchUser(id: number): Promise<MyApiResult<MyUser>> {
+
+// fetch data from API using async function
+async function fetchUserAsync(id: number): Promise<MyApiResult<MyUser>> {
     // make use of string template literals using string interpolation
     const fetchUrl: string = `https://reqres.in/api/users/${id}`;
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await
+    // the await expression causes async code to wait until promise is settled (fulfilled or rejected)
+    // when resumed, the value of the await expression is that of the fulfilled promise
     const response = await fetch(fetchUrl);
     const json: unknown = await response.json();
     assertUserApiResult(json);
     return json;
 }
 
-// main code
+// fetch data from API using promise chain
+function fetchUserPromiseChain(id: number): Promise<MyApiResult<MyUser>> {
+    const fetchUrl: string = `https://reqres.in/api/users/${id}`;
+    return fetch(fetchUrl)
+        .then(response => response.json())
+        .then(json => {
+            assertUserApiResult(json);
+            return json;
+        })
+}
 
-fetchUser(2).then(result => console.log(result.data.email));
-fetchUser(3).then(result => console.log(result.data.email));
-fetchUser(4).then(result => console.log(result.data.email));
+// main code
+function main() {
+    let i: number = 1;
+    let max: number = 6;
+    while (i < max) {
+        fetchUserAsync(i).then(result => console.log("from async function " + result.data.id + " " + result.data.email));
+        i++;
+    }
+
+    i = 1;
+    while (i < max) {
+        fetchUserPromiseChain(i).then(result => console.log("from promise chain " + result.data.id + " " + result.data.email));
+        i++;
+    }
+
+}
+
+main();
